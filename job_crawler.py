9efeea398ea7f2950c92936756ed21c719659bea
Peetu Skyttä | pskytta <77061872+peetuskytta@users.noreply.github.com>
2023-05-18 08:32:44 +0300

    First commit
    Added the .py file

---
+++ job_crawler.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+
+index = 2
+baseurl = 'https://duunitori.fi/tyopaikat?filter_work_type=full_time&haku=system%20specialist%3Bdeveloper%3BLinux-kehitt%C3%A4j%C3%A4%3Bohjelmoija'
+page_found = True
+page_number = 1
+
+while page_found:
+	if page_number == 1:
+		response = requests.get(baseurl)
+	else:
+		url = baseurl + "&sivu=" + str(index)
+		index += 1
+	if response.status_code == 200:
+
+	if response.status_code == 404:
+		print(f"Page {page_number}")
+
+html = response.content
+
+soup = BeautifulSoup(html, 'html.parser')
+
+div_outer = soup.find_all('h3', class_='job-box__title')
+for h3 in div_outer:
+	print(h3)
+	#print(div_outer)
