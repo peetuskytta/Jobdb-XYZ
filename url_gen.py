d1a1de8f04e7f6c9e26a23ceff74ef2f212e1621
Alero <alero@Ashanti.local>
2023-05-26 17:07:00 +0300

    First version of url_generator. Takes list of titles from a 'job_titles'-file. Converts certain special characters so they are compitable with url, turns them in to a string and attaches it at the end of base url.

--- url_gen.py
+++ url_gen.py
@@ -1,8 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
 
-file = open("testfile.txt", "r")
-print(file.readlines())
-print()
-file.seek(0)
-print(file.read())
+
+##'https://duunitori.fi/tyopaikat?filter_work_type=full_time&haku=system%20specialist%3Bdeveloper%3BLinux-kehitt%C3%A4j%C3%A4%3Bohjelmoija'
+
+
+def url_gen():
+    base = 'https://duunitori.fi/tyopaikat?filter_work_type=full_time&haku='
+    file = open("job_titles", "r")
+##  print(file.readlines())
+##  file.seek(0)
+##  print(file.read())
+##  file.seek(0)
+    string = "".join(file)
+    for symbol in "ö":
+        string = string.replace(symbol, "%C3%B6")
+    for symbol in "ä":
+        string = string.replace(symbol, "%C3%A4")
+    for symbol in " ":
+        string = string.replace(symbol, "%20")
+    for symbol in "\n":
+        string = string.replace(symbol, "%3B")
+    ret = base + string[:-3]
+    return ret
