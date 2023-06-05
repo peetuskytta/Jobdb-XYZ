18cc7f3505cbe465921d83ed7308ca3755ae14f7
peetuskytta <peetu.skytta@gmail.com>
2023-06-05 17:17:19 +0300

    Updated the way data was stored to actually use the BS4 to its full potential in the save job function

--- sources/utility_functions.py
+++ sources/utility_functions.py
@@ -7,11 +7,16 @@ from classes import Job
 from bs4 import BeautifulSoup
 
 def save_job(data, url):
-    # the text.strip will remove any leading or trailing whitespaces
-    job_title = data.find('h3', class_='job-box__title').text.strip()
-    a_ref = data.find('a')
-    job_id = a_ref.get('data-id')
-    job_link = url + a_ref.get('href')
+    # OLD WAY
+    	#job_title = data.find('h3', class_='job-box__title').text.strip()
+    	#a_ref = data.find('a')
+    	#job_id = a_ref.get('data-id')
+    	#job_link = url + a_ref.get('href')
+    #
+    # NEW WAY
+    job_title = data.h3.text
+    job_id = data.a['data-id']
+    job_link = url + data.a['href']
     new_job = Job(job_title, job_id, job_link)
     return new_job
 
@@ -28,17 +33,17 @@ def testAndActConnection(db_name, jobs_list):
         cursor = sqlConnection.cursor()
         # The table users will store the email and user_id. Later we can relate user_id with multiple
         # job_ids to identify what the user has received already
-        cursor.execute("CREATE TABLE IF NOT EXISTS users (user_id INT PRIMARY KEY, email VARCHAR(255))")
-        cursor.execute("CREATE TABLE IF NOT EXISTS jobs (job_id INT PRIMARY KEY, title TEXT, link TEXT)")
+        #cursor.execute("CREATE TABLE IF NOT EXISTS users (user_id INT PRIMARY KEY, email VARCHAR(255))")
+        cursor.execute("CREATE TABLE IF NOT EXISTS jobs (id INT PRIMARY KEY, title TEXT, link TEXT)")
 
-        cursor.execute("SELECT job_id FROM jobs")
+        cursor.execute("SELECT id FROM jobs")
         rows = cursor.fetchall()  # fetches all the job_id rows to be checked for existing ones
         compareIds = [row[0] for row in rows]
 
         for job in jobs_list:
             if job.id in compareIds:
                 continue
-            query = "INSERT INTO jobs (job_id, title, link) VALUES (?, ?, ?)"
+            query = "INSERT INTO jobs (id, title, link) VALUES (?, ?, ?)"
             values = (job.id, job.title, job.url)
             cursor.execute(query, values)
             compareIds.append(job.id)
