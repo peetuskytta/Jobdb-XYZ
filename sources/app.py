a350b876f1e729e8e0e31dc788643bdc5691f1a5
peetuskytta <peetu.skytta@gmail.com>
2023-08-14 13:49:03 +0300

    modified app.py file and corrected the index.htlm location for flask to find it.

--- sources/app.py
+++ sources/app.py
@@ -2,22 +2,22 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        keywords = request.form.get('keywords').split(',')
+
+        # add your keyword processing logic here
+        print("Email:", email)
+        print("Keywords:", keywords)
+        # You can send an email with the processed result here
+
     return render_template('index.html')
 
-@app.route('/process_keywords', methods=['POST'])
-def process_keywords():
-    email = request.form.get('email')
-    keywords = request.form.get('keywords').split(',')
+if __name__ == '__main__':
+    app.run(debug=True)
 
-    # add keyword processing logic here
-    print("Email:", email)
-    print("Keywords:", keywords)
-
-    # You can send an email with the processed result here
-
-    return "Keywords processed successfully!"
 
 if __name__ == '__main__':
     app.run(debug=True)
