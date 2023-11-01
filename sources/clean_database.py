6dda7142b57eef2460ef40e3851a86dddc0c8775
peetuskytta <peetu.skytta@gmail.com>
2023-11-01 13:13:58 +0200

    database cleaner updated.

--- sources/clean_database.py
+++ sources/clean_database.py
@@ -9,20 +9,18 @@ database = "/opt/database/jobs.db"
 
 def checkForOld(links: dict) -> list:
     toDelete = [] # Store the ID to be removed from Database
-    counter = 0
     for key, value in links.items():
-        response = requests.get(value)
+        #print(key)  # Print the key (ID) for debugging
         if value == "ERROR":
             toDelete.append(key)
-            counter += 1
-        elif response.status_code == 200:
-            html = response.content
-            soup = BeautifulSoup(html, 'html.parser')
-            grid = soup.find_all('h2', class_='text--warning')
-            if grid:
-                toDelete.append(key)
-                counter += 1
-    print(f"Number of items to be deleted from the database: {counter}")
+        else:
+            response = requests.get(value)
+            if response.status_code == 200:
+                html = response.content
+                soup = BeautifulSoup(html, 'html.parser')
+                grid = soup.find_all('h2', class_='text--warning')
+                if grid:
+                    toDelete.append(key)
     return toDelete
 
 
@@ -32,31 +30,30 @@ def open_database(db_name):
         sqlConnection = db.connect(db_name)
         cursor = sqlConnection.cursor()
         cursor.execute("""
-            SELECT link, id, title FROM jobs;
+            SELECT link, id, title, category FROM jobs;
             """)
         rows = cursor.fetchall()
         linksToTest = {} # Collect ID and link
         errorCases = ["C-kortillinen", "C-kortillisia", "hoitaja", "Esperi", "sairaanhoidon"]
         for row in rows:
-            link_val, id_val, title = row
+            link_val, id_val, title, category = row
             if row[2] in errorCases:
+                linksToTest[id_val] = "ERROR"
+            if row[3] == "c on r ":
                 linksToTest[id_val] = "ERROR"
             else:
                 linksToTest[id_val] = link_val
-
         toDelete = checkForOld(linksToTest)
-
-        for id in toDelete:
-            cursor.execute("SELECT id FROM jobs WHERE id = ?", (id,))
+        for item in toDelete:
+            cursor.execute("SELECT id FROM jobs WHERE id = ?", (item,))
             existing_id = cursor.fetchone()
             if existing_id:
-                cursor.execute("DELETE FROM jobs WHERE id = ?", (id,))
-                print("Deleted")
+                cursor.execute("DELETE FROM jobs WHERE id = ?", (item,))
         sqlConnection.commit()
         cursor.close()
 
     except db.Error as error:
-        print("Error while connecting to SQLite database: ", error)
+        print("Error while connecting to SQLite database while cleaning: ", error)
         return
 
     finally:
