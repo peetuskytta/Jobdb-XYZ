2fba9ec86268f9f3cb0b96ed47099302865a1d68
peetuskytta <peetu.skytta@gmail.com>
2023-11-07 22:03:07 +0200

    simplified the cleaning by separating non-relevant clean and old post cleaning, the latter being under construction

--- crawler/clean_database.py
+++ crawler/clean_database.py
@@ -5,56 +5,30 @@ import requests
 # TEST THIS LINK FOR EXAMPLE:
 # https://duunitori.fi/tyopaikat/tyo/senior-java-developer-relocation-to-switzerland-scsom-14504783
 
-def checkForOld(links: dict) -> list:
-    toDelete = [] # Store the ID to be removed from Database
-    for key, value in links.items():
-        #print(key)  # Print the key (ID) for debugging
-        #way to distinguish between jobs from different websites (jobly not supported yet)
-        if value == "ERROR":
-            toDelete.append(key)
-        else:
-            response = requests.get(value)
-            if response.status_code == 200:
-                html = response.content
-                soup = BeautifulSoup(html, 'html.parser')
-                grid = soup.find_all('h2', class_='text--warning')
-                if grid:
-                    toDelete.append(key)
-    return toDelete
 
+### This function will load delete_pattern file content from files folder to search_patterns
+### variable (list) which is used to delete specific patterns not wanted in the database.
 def clean_database():
     sqlConnection = None
-    print("Starting database cleaning")
+    print("Starting database cleaning based on patterns")
     try:
         sqlConnection = db.connect("../database/jobs.db")
         cursor = sqlConnection.cursor()
-        cursor.execute("""
-            SELECT link, id, title, category FROM jobs;
-            """)
-        rows = cursor.fetchall()
-        linksToTest = {}
-        errorCases = ["c-kort", "hoitaj", "esperi", "sairaanhoidon", "myyjiä", "myynti", "myyjä", "lukkoseppä", "fashion", "sosiaali", "inside sales", "attorney", "legal", "marketing", "ecommerce manager", "sähkösuunnittelija", "projektipäälli", "luottoriskianalyytikko", "supervisor/commissioning", "talent acquisition", "doctoral research", "treasury"]
-        for row in rows:
-            link_val, id_val, title, category = row
-            if row[2].lower() in errorCases:
-                linksToTest[id_val] = "ERROR"
-            if row[3] == "c on r" or row[3] == "go":
-                linksToTest[id_val] = "ERROR"
-            else:
-                linksToTest[id_val] = link_val
-        toDelete = checkForOld(linksToTest)
-        for item in toDelete:
-            cursor.execute("SELECT id FROM jobs WHERE id = ?", (item,))
-            existing_id = cursor.fetchone()
-            if existing_id:
-                cursor.execute("DELETE FROM jobs WHERE id = ?", (item,))
-        cursor.execute("DELETE FROM jobs WHERE title LIKE '%C-kor%'")
-        cursor.execute("DELETE FROM jobs WHERE title LIKE '%C-l %'")
+
+        # open and split content of the file
+        with open("files/delete_pattern", 'r') as file:
+            search_patterns = file.read().splitlines()
+
+        # loop the patterns and execute sql DELETE
+        for pattern in search_patterns:
+            sql = f"DELETE FROM jobs WHERE title LIKE '{pattern}'"
+            cursor.execute(sql)
+
         sqlConnection.commit()
         cursor.close()
 
     except db.Error as error:
-        print("Error while connecting to SQLite database while cleaning: ", error)
+        print("Error connecting to SQLite database while cleaning: ", error)
         return
 
     finally:
@@ -62,5 +36,9 @@ def clean_database():
             sqlConnection.close()
         return
 
-# if you want to run this separately
+# if you want to run this separately remove the comment on the next line and run the script with command: 'python3 clean_database.py'
 #clean_database()
+
+### Make a separate cleaner for out of date job postings.
+### Tricky to make work for multiple different job sites.
+### Consider deleting any posting that has been in database for more than a month.
