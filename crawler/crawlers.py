507342a8b3f45eba55ee6643d82926ae17f83150
peetuskytta <peetu.skytta@gmail.com>
2023-11-09 14:38:48 +0200

    Main merged to peetu branch and updated cleaner and crawlers

--- crawler/crawlers.py
+++ crawler/crawlers.py
@@ -36,18 +36,18 @@ def duunitori_crawler():
                 raise ConnectionError("Under maintenance")
             if response.status_code == 404:
                 page_found = False
+            print(f"Response status: {response.status_code}")
 
         # Should be commented out in production as there's no GUI in VM.
-        sys.stdout.flush()
-        sys.stdout.write("\rNumber of pages processed: %d" % page_number)
+        #sys.stdout.flush()
+        #sys.stdout.write("\rNumber of pages processed: %d" % page_number)
 
         response = None
         page_number += 1
 
     # Should be commented out in production as there's no GUI in VM
     database_inserts(duunitori_jobs)
-    print(f"\nDuunitori crawl: {len(duunitori_jobs)} jobs added")
-
+    print(f"\nDuunitori crawl: {len(duunitori_jobs)}")
 
 
 ### Crawler to find the job postings in Jobly.fi website. ###
